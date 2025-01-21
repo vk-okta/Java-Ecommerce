fc474af5a95e472348da5eb42d668c1d53913ddb
vivekokta <vivek.giri@okta.com>
2025-01-21 19:33:34 +0530

    First Commit

---
+++ src/main/java/com/ecommerce/javaecom/JavaEcomApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.javaecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEcomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEcomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/javaecom/JavaEcomApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ecommerce.javaecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEcomApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
