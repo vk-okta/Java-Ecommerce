36bc076cbf0a7e48e221fea83feacea7da47be3d
vivekokta <vivek.giri@okta.com>
2025-01-23 23:03:31 +0530

    Lombok added

--- src/main/java/com/ecommerce/javaecom/model/Category.java
+++ src/main/java/com/ecommerce/javaecom/model/Category.java
@@ -4,10 +4,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // this annotation marks this Java class as an entity that should be mapped to a table in the database.
 // the name of the table will be same as class name
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,30 +24,32 @@ public class Category {
     // TODO
     // find out why it is a good practice to have
     // a default constructor in the Entity
-    public Category() {
-    }
+    //public Category() {
+    //}
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
+    // all the constructors, getters,setters will be added by lombok
+
+    //public Category(Long categoryId, String categoryName) {
+    //    this.categoryId = categoryId;
+    //    this.categoryName = categoryName;
+    //}
 
     // Getters and setters are used to set the value in db
     // and that fetch the value from db
 
-    public String getCategoryName() {
-        return categoryName;
-    }
+    //public String getCategoryName() {
+    //    return categoryName;
+    //}
 
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
+    //public void setCategoryName(String categoryName) {
+    //    this.categoryName = categoryName;
+    //}
 
-    public Long getCategoryId() {
-        return categoryId;
-    }
+    //public Long getCategoryId() {
+    //    return categoryId;
+    //}
 
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
+    //public void setCategoryId(Long categoryId) {
+    //    this.categoryId = categoryId;
+    //}
 }
